dce1ce2844300222766b6a6fb60f7a8294e48048
Jeshin Kim <37035547+jason0904@users.noreply.github.com>
2025-11-06 15:00:13 +0000

    refactor(user): 유저에 이름 넣을 수 있게 struct 수정

--- user.go
+++ user.go
@@ -9,13 +9,15 @@ import (
 type User struct {
 	conn *websocket.Conn
 	ID   string
+	Name string
 	game *Game //자신이 속한 게임 방에 관련된 참조
 }
 
-func NewUser(conn *websocket.Conn, ID string) *User {
+func NewUser(conn *websocket.Conn, ID string, Name string) *User {
 	return &User{
 		conn: conn,
 		ID:   ID,
+		Name: Name,
 	}
 }
 
@@ -30,11 +32,11 @@ func (u *User) readLoop() {
 		if err != nil {
 			//정상적으로 종료되었는지 체크
 			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
-				log.Printf("Read error for client %s: %v", u.ID, err)
+				log.Printf("Read error for client %s: %v", u.Name, err)
 			}
 			break
 		}
-		log.Printf("Received message from %s: %s", u.ID, string(msg))
+		log.Printf("Received message from %s: %s", u.Name, string(msg))
 		// 메시지 처리 로직 추가
 		u.game.handleMessage(u, msg)
 	}
