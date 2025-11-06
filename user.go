fdad4ebb2b4464cbc35b86c5a8cb1653132b1f02
Jeshin Kim <37035547+jason0904@users.noreply.github.com>
2025-11-06 10:42:58 +0000

    feat(user):유저 객체의 출력 구현

---
+++ user.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gofiber/contrib/websocket"
+)
+
+type User struct {
+	conn *websocket.Conn
+	name string
+	game *Game //자신이 속한 게임 방에 관련된 참조
+}
+
+func NewUser(conn *websocket.Conn, name string) *User {
+	return &User{
+		conn: conn,
+		name: name,
+	}
+}
+
+func (u *User) readLoop() {
+	defer func() {
+		//정상 종료 로직 추가.
+	}
+
+	for {
+		_, msg, err := u.conn.ReadMessage()
+		if err != nil {
+			//정상적으로 종료되었는지 체크
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+				log.Printf("Read error for client %s: %v", u.name, err)
+			}
+			break
+		}
+		log.Printf("Received message from %s: %s", u.name, string(msg))
+		// 메시지 처리 로직 추가
+		u.game.handleMessage(u, msg)
+	}
+}
