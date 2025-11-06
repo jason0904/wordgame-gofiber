e50fd92e0a3d0e8ba12a0db447925b53093f1779
Jeshin Kim <37035547+jason0904@users.noreply.github.com>
2025-11-06 13:39:30 +0000

    feat(room):유저의 입장과 퇴장을 관리, 메시지를 쓰고, 읽는 기능 구현

---
+++ room.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"log"
+	"sync"
+
+	"github.com/gofiber/contrib/websocket"
+)
+
+type Room struct {
+	clients    map[*User]bool
+	broadcast  chan []byte
+	register   chan *User
+	unregister chan *User
+	mu         sync.RWMutex
+}
+
+func NewRoom() *Room {
+	return &Room{
+		clients:    make(map[*User]bool),
+		broadcast:  make(chan []byte),
+		register:   make(chan *User),
+		unregister: make(chan *User),
+	}
+}
+
+func (r *Room) run() {
+	for {
+		select {
+		case user := <-r.register:
+			r.mu.Lock()
+			r.clients[user] = true
+			r.mu.Unlock()
+			log.Printf("User %s joined the room.", user.ID)
+		case user := <-r.unregister:
+			r.mu.Lock()
+			if _, ok := r.clients[user]; ok {
+				delete(r.clients, user)
+				log.Printf("User %s left the room.", user.ID)
+			}
+			r.mu.Unlock()
+		case message := <-r.broadcast:
+			r.mu.RLock()
+			for client := range r.clients {
+				err := client.conn.WriteMessage(websocket.TextMessage, message)
+				if err != nil {
+					log.Printf("Error broadcasting to %s: %v", client.ID, err)
+					client.conn.Close()
+					delete(r.clients, client)
+				}
+			}
+			r.mu.RUnlock()
+		}
+	}
+}
