ef688fae142d88b2b37dfc4f5d28c599edab9dfc
Jeshin Kim <37035547+jason0904@users.noreply.github.com>
2025-11-07 15:06:28 +0000

    fix(db):db 테이블에 접근하지 못하던 문제 수정

--- db.go
+++ db.go
@@ -1,11 +1,13 @@
 package main
 
 import (
-	"context"
+	"log"
 
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
+
+var DB *gorm.DB
 
 type Word struct {
 	Id   int    `gorm:"column:id"`
@@ -13,25 +15,30 @@ type Word struct {
 	Part string `gorm:"column:part"`
 }
 
+// GORM에게 kr테이블을 사용함을 알려줌.
+func (Word) TableName() string {
+	return "kr"
+}
+
 func IsWordInDB(word string) bool {
-	db, err := initDB()
-	if err != nil {
+	if DB == nil {
+		log.Println("Database is not initialized.")
 		return false
 	}
 
 	var result Word
-	err = db.WithContext(context.Background()).Where("word = ?", word).First(&result).Error
+	err := DB.Where("word = ?", word).First(&result).Error
 
 	return err == nil
 }
 
-// 비공개 메서드
-
-func initDB() (*gorm.DB, error) {
+// InitDB 함수는 한번만 호출되게 해야함. game에서 해야하나..
+func InitDB() {
 	//db 출처 https://github.com/korean-word-game/db?tab=readme-ov-file
 	db, err := gorm.Open(sqlite.Open("kr_korean.db"), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
 	}
-	return db, nil
+	DB = db
+	log.Println("Database connection successfully established.")
 }
