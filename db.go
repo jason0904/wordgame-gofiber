7d39b6438a3d12054b179546af13c92248cb356f
Jeshin Kim <37035547+jason0904@users.noreply.github.com>
2025-11-07 14:52:05 +0000

    feat(db):db연결과 db에 해당 단어 있는지 여부 확인 기능 추가

---
+++ db.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+  "context"
+  "gorm.io/driver/sqlite"
+  "gorm.io/gorm"
+)
+
+type Word struct {
+	id int
+	word string
+	part string
+}
+
+func IsWordInDB(word string) bool {
+	db, err := initDB()
+	if err != nil {
+		return false
+	}
+	
+	var result Word
+	err = db.WithContext(context.Background()).Where("word = ?", word).First(&result).Error
+
+	return err == nil
+}
+
+// 비공개 메서드
+
+func initDB() (*gorm.DB, error) {
+	//db 출처 https://github.com/korean-word-game/db?tab=readme-ov-file
+	db, err := gorm.Open(sqlite.Open("kr_korean.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+	return db, nil
+}
