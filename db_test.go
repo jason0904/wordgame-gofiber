033d1f045eed9986a3ca6eebaf1420c66f7ce683
Jeshin Kim <37035547+jason0904@users.noreply.github.com>
2025-11-08 04:25:46 +0000

    test(db):글자수에 맞는 단어 랜덤으로 하나 가져오는 기능 테스트

--- db_test.go
+++ db_test.go
@@ -43,3 +43,33 @@ func TestIsWordInDB(t *testing.T) {
 		})
 	}
 }
+
+func TestGetRandomWordByLength(t *testing.T) {
+	testCases := []struct {
+		name        string
+		length      int
+		expectError bool
+	}{
+		{
+			name:        "길이 2의 단어 가져오기",
+			length:      2,
+			expectError: false,
+		},
+		{
+			name:        "길이 5의 단어 가져오기",
+			length:      5,
+			expectError: false,
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			word, err := GetRandomWordByLength(tc.length)
+			if tc.expectError {
+				assert.Error(t, err, "오류가 예상되었으나 발생하지 않았습니다.")
+			} else {
+				assert.NoError(t, err, "오류가 발생했습니다: %v", err)
+				assert.Equal(t, tc.length, len([]rune(word)), "가져온 단어의 길이가 예상과 다릅니다.")
+			}
+		})
+	}
+}
