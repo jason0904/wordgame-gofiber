a4a9a81b3529aa25c37498f1f38ca68d1ad0924f
Jeshin Kim <37035547+jason0904@users.noreply.github.com>
2025-11-14 10:37:11 +0000

    refactor(constant):메시지 추가

--- internal/game/constant.go
+++ internal/game/constant.go
@@ -19,10 +19,11 @@ const (
     ALLEXITMSG           = "모든 플레이어가 나갔습니다. 새로운 플레이어를 기다립니다."
     CURRENTTURNMSG       = "님의 차례입니다."
 
-    GAMEALREADYSTARTEDMSG    = "이미 게임이 시작되었습니다."
+    GAMEALREADYSTARTEDMSG = "이미 게임이 시작되었습니다."
     NOHOSTPRIVILEGESMSG  = "게임을 시작할 권한이 없습니다. 호스트만 게임을 시작할 수 있습니다."
     MINPLAYERTOSTARTMSG  = "게임을 시작하려면 최소 %d명의 플레이어가 필요합니다."
-    NOTTOHANDLEPLAYMSG = "현재 게임이 시작되지 않았으므로 단어를 제출할 수 없습니다."
+    NOTTOHANDLEPLAYMSG   = "현재 게임이 시작되지 않았으므로 단어를 제출할 수 없습니다."
+    NOTCURRENTPLAYERSMSG = "현재 당신의 차례가 아닙니다."
 
     TYPEWORDMSG        = "단어를 입력하세요."
     MINWORDLENGTHMSG   = "단어는 최소 2자 이상이어야 합니다."
