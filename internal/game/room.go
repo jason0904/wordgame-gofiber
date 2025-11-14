f10196de303cacae4b436dfa08a9343a069d10fb
Jeshin Kim <37035547+jason0904@users.noreply.github.com>
2025-11-14 08:19:00 +0000

    refactor(room):구현 길이가 긴 함수 분리

--- internal/game/room.go
+++ internal/game/room.go
@@ -26,30 +26,46 @@ func NewRoom() *Room {
 
 func (r *Room) Run() {
 	for {
-		select {
-		case user := <-r.register:
-			r.mu.Lock()
-			r.clients[user] = true
-			r.mu.Unlock()
-			log.Printf("User %s joined the room.", user.Name)
-		case user := <-r.unregister:
-			r.mu.Lock()
-			if _, ok := r.clients[user]; ok {
-				delete(r.clients, user)
-				log.Printf("User %s left the room.", user.Name)
-			}
-			r.mu.Unlock()
-		case message := <-r.broadcast:
-			r.mu.RLock()
-			for client := range r.clients {
-				err := client.conn.WriteMessage(websocket.TextMessage, message)
-				if err != nil {
-					log.Printf("Error broadcasting to %s: %v. Closing connection.", client.Name, err)
-					client.conn.Close()
-					delete(r.clients, client)
-				}
-			}
-			r.mu.RUnlock()
+		r.handleConnection()
+	}
+}
+
+func (r *Room) handleConnection() {
+	select {
+	case user := <-r.register:
+		r.handleRegister(user)
+	case user := <-r.unregister:
+		r.handleUnregister(user)
+	case message := <-r.broadcast:
+		r.broadcastMessage(message)
+	}
+}
+
+func (r *Room) handleRegister(user *User) {
+	r.mu.Lock()
+	r.clients[user] = true
+	r.mu.Unlock()
+	log.Printf("User %s joined the room.", user.Name)
+}
+
+func (r *Room) handleUnregister(user *User) {
+	r.mu.Lock()
+	if _, ok := r.clients[user]; ok {
+		delete(r.clients, user)
+		log.Printf("User %s left the room.", user.Name)
+	}
+	r.mu.Unlock()
+}
+
+func (r *Room) broadcastMessage(message []byte) {
+	r.mu.RLock()
+	for client := range r.clients {
+		err := client.conn.WriteMessage(websocket.TextMessage, message)
+		if err != nil {
+			log.Printf("Error broadcasting to %s: %v. Closing connection.", client.Name, err)
+			client.conn.Close()
+			r.handleUnregister(client)
 		}
 	}
+	r.mu.RUnlock()
 }
