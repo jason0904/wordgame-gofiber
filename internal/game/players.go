f85d2772ee793fddbf6e5936c4745939010b3d4a
Jeshin Kim <37035547+jason0904@users.noreply.github.com>
2025-11-14 07:56:24 +0000

    refactor(players):구현길이가 긴 함수 분리

--- internal/game/players.go
+++ internal/game/players.go
@@ -2,76 +2,28 @@ package game
 
 import "log"
 
-// ...existing code...
 func (g *Game) addUser(user *User) {
 	g.mu.Lock()
 	defer g.mu.Unlock()
-	for _, u := range g.players {
-		if u.ID == user.ID {
-			return //이미 존재하는 사용자
-		}
-	}
+	g.checkPlayerExist(user)
 	g.players = append(g.players, user)
 
 	if len(g.players) == 1 {
-		g.hostUserId = user.ID
-		log.Printf(HOSTLOGMSG, user.Name, user.ID)
-		g.reset()
+		g.handleRoomInit(user)
 	}
 	log.Printf(ENTERPLAYERLOGMSG, user.Name, user.ID)
 }
 
 func (g *Game) removeUser(user *User) {
 	g.mu.Lock()
-	shouldDelete := false
-
 	for i, p := range g.players {
-		if p.ID == user.ID {
-			g.players = append(g.players[:i], g.players[i+1:]...)
-			log.Printf(EXITPLAYERLOGMSG, user.Name, user.ID)
-
-			if g.hostUserId == user.ID {
-				//유저 아무에게 호스트 권한 이전
-				if len(g.players) > 0 {
-					randomUser := g.players[g.random.MakeRandomNumber(0, len(g.players))].ID
-					g.hostUserId = randomUser
-					log.Printf(HOSTCHANGELOGMSG, randomUser)
-				} else {
-					g.hostUserId = ""
-				}
-			}
-
-			if g.currentUserID == user.ID && len(g.players) > 0 && !g.gameover {
-				nextPlayerIndex := i % len(g.players)
-				g.currentUserID = g.players[nextPlayerIndex].ID
-				g.message = EXITMSG + g.currentUserID
-			} else if len(g.players) == 0 {
-				g.currentUserID = ""
-				g.message = ALLEXITMSG
-				g.lastWord = ""
-				g.usedWords = make(map[string]bool)
-				g.gameover = false
-				shouldDelete = true
-			}
-			g.mu.Unlock()
-
-			// 데드락 가능성 때문에 언락후 삭제.
-			if shouldDelete && g.manager != nil {
-				log.Printf(DELETEROOMLOGMSG, g.RoomId)
-				g.manager.DeleteRoom(g.RoomId)
-			}
-			return
-		}
+		g.handleDeleteUser(p, user, i)
 	}
-
 	for i, s := range g.spectators {
-		if s.ID == user.ID {
-			g.spectators = append(g.spectators[:i], g.spectators[i+1:]...)
-			log.Printf(REMOVESPECTATORLOGMSG, user.Name, user.ID)
-			break
-		}
+		g.handleDeleteSpectator(s, user, i)
 	}
 	g.mu.Unlock()
+	g.deleteRoom()
 }
 
 func (g *Game) setNextPlayerTurn(currentUserID string) {
@@ -88,5 +40,73 @@ func (g *Game) setNextPlayerTurn(currentUserID string) {
 			g.message = g.makeNameToDisplay(g.currentUserID, nextPlayerName) + CURRENTTURNMSG
 			return
 		}
+	}
+}
+
+func (g *Game) handleRoomInit(user *User) {
+	g.hostUserId = user.ID
+	log.Printf(HOSTLOGMSG, user.Name, user.ID)
+	g.reset()
+}
+
+func (g *Game) checkPlayerExist(user *User) {
+	for _, u := range g.players {
+		if u.ID == user.ID {
+			return //이미 존재하는 사용자
+		}
+	}
+}
+
+func (g *Game) handleDeleteUser(target, user *User, index int) {
+
+	if target.ID == user.ID {
+		g.players = append(g.players[:index], g.players[index+1:]...)
+		log.Printf(EXITPLAYERLOGMSG, user.Name, user.ID)
+		g.handleHostLeft(user)
+		g.makeNewPlayerTurn(user, index)
+	}
+}
+
+func (g *Game) handleDeleteSpectator(target, user *User, index int) {
+	if target.ID == user.ID {
+		g.spectators = append(g.spectators[:index], g.spectators[index+1:]...)
+		log.Printf(REMOVESPECTATORLOGMSG, user.Name, user.ID)
+	}
+}
+
+func (g *Game) handleHostLeft(user *User) {
+	if g.hostUserId == user.ID {
+		g.makeNewHost()
+	}
+}
+
+func (g *Game) makeNewHost() {
+	if len(g.players) > 0 {
+		randomUser := g.players[g.random.MakeRandomNumber(0, len(g.players))].ID
+		g.hostUserId = randomUser
+		log.Printf(HOSTCHANGELOGMSG, randomUser)
+	} else {
+		g.hostUserId = ""
+	}
+}
+
+func (g *Game) makeNewPlayerTurn(user *User, index int) {
+	if g.currentUserID == user.ID && len(g.players) > 0 && !g.gameover {
+		nextPlayerIndex := index % len(g.players)
+		g.currentUserID = g.players[nextPlayerIndex].ID
+		g.message = EXITMSG + g.currentUserID
+	} else if len(g.players) == 0 {
+		g.currentUserID = ""
+		g.message = ALLEXITMSG
+		g.lastWord = ""
+		g.usedWords = make(map[string]bool)
+		g.gameover = false
+	}
+}
+
+func (g *Game) deleteRoom() {
+	if len(g.players) == 0 {
+		log.Printf(DELETEROOMLOGMSG, g.RoomId)
+		g.manager.DeleteRoom(g.RoomId)
 	}
 }
